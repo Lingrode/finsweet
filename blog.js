e17a5804a287b55d1364d7d3e5773abf4bf3b2e5
Lingrode <illia050@ukr.net>
2024-12-06 06:51:31 +0000

    Deploying to gh-pages from @ Lingrode/finsweet@98af13ff0343e2c3aef755caf3253b0dbdcd7a5d 🚀

--- blog.js
+++ blog.js
@@ -1,8 +1,8 @@
-import"./assets/footer-jTjVQDYj.js";import{a}from"./assets/vendor-BQLDhyXy.js";const r=document.querySelectorAll(".posts__item"),n="https://jsonplaceholder.org";for(const t of r)console.log(t);const i=async()=>(await a(`${n}/posts`)).data,l=t=>{const{title:e,content:s,category:o}=t,c=s.split(".").slice(0,3).join(".");return`
+import"./assets/footer-Dg_39VAe.js";import{a}from"./assets/vendor-BQLDhyXy.js";const n=document.querySelectorAll(".posts__item"),r=document.querySelector(".category__title"),l="https://jsonplaceholder.org",i=window.location.pathname;i==="/blog.html"&&(r.textContent="All Categories",r.style.textAlign="left");const p=async()=>(await a(`${l}/posts`)).data,m=t=>{const{title:s,content:e,category:o}=t,c=e.split(".").slice(0,3).join(".");return`
     <div class="posts__item-wrapper">
       <a class="posts__item-subtitle" href="#">${o}</a>
-      <h3 class="posts__item-title"><a href="#">${e}</a></h3>
+      <h3 class="posts__item-title"><a href="#">${s}</a></h3>
       <p class="posts__item-text">${c}</p>
     </div>
-  `},p=async()=>{try{const e=(await i()).filter(s=>s.id<=5);r.forEach((s,o)=>{if(e[o]){const c=l(e[o]);s.insertAdjacentHTML("beforeend",c)}})}catch(t){console.log(t)}};p();
+  `},_=async()=>{try{const s=(await p()).filter(e=>e.id<=5);n.forEach((e,o)=>{if(s[o]){const c=m(s[o]);e.insertAdjacentHTML("beforeend",c)}})}catch(t){console.log(t)}};_();
 //# sourceMappingURL=blog.js.map

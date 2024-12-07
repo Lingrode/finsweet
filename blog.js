f0acfef972beb2580d803875ad8900df6d78b60c
Lingrode <illia050@ukr.net>
2024-12-07 03:14:45 +0000

    Deploying to gh-pages from @ Lingrode/finsweet@acfc80cd3697cac8806905b44344cb8c71f325df 🚀

--- blog.js
+++ blog.js
@@ -1,4 +1,4 @@
-import"./assets/footer-1dPdjaEL.js";import{a}from"./assets/vendor-BQLDhyXy.js";const n=document.querySelectorAll(".posts__item"),r=document.querySelector(".category__title"),l="https://jsonplaceholder.org",i=window.location.pathname;i==="/blog.html"&&(r.textContent="All Categories",r.style.textAlign="left");const p=async()=>(await a(`${l}/posts`)).data,m=t=>{const{title:s,content:e,category:o}=t,c=e.split(".").slice(0,3).join(".");return`
+import"./assets/footer-BKmWx1tH.js";import{a}from"./assets/vendor-BQLDhyXy.js";const n=document.querySelectorAll(".posts__item"),r=document.querySelector(".category__title"),l="https://jsonplaceholder.org",i=window.location.pathname;i==="/blog.html"&&(r.textContent="All Categories",r.style.textAlign="left");const p=async()=>(await a(`${l}/posts`)).data,m=t=>{const{title:s,content:e,category:o}=t,c=e.split(".").slice(0,3).join(".");return`
     <div class="posts__item-wrapper">
       <a class="posts__item-subtitle" href="#">${o}</a>
       <h3 class="posts__item-title"><a href="#">${s}</a></h3>
